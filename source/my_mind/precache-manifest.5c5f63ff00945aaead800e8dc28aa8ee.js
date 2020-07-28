ba684ea5af6d24ea2e14c086cf28bbf19f4c18c4
Yeonggyun Kim <kygha7205@naver.com>
2020-07-28 16:26:17 +0900

    Add "my_mind" meme generator

---
+++ source/my_mind/precache-manifest.5c5f63ff00945aaead800e8dc28aa8ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "736bd46392ba2b6f3668bd17efb7a363",
+    "url": "/my_mind/index.html"
+  },
+  {
+    "revision": "06d905e8094fe24b439c",
+    "url": "/my_mind/static/js/2.411b2ab5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my_mind/static/js/2.411b2ab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d5403a468805dab523d",
+    "url": "/my_mind/static/js/main.7f7e7266.chunk.js"
+  },
+  {
+    "revision": "9a319686e2166b6818d3",
+    "url": "/my_mind/static/js/runtime-main.50fa81b9.js"
+  },
+  {
+    "revision": "ad7562d763896fae02a3e16cd2478bdb",
+    "url": "/my_mind/static/media/taxi_edit.ad7562d7.png"
+  }
+]);
